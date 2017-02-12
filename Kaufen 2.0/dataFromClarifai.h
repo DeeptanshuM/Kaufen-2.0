9fb70f61b4c2f3ee1205359fb1b2181d79b671ea
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-02-11 19:34:56 -0500

    starting again

---
+++ Kaufen 2.0/dataFromClarifai.h
@@ -0,0 +1,13 @@
+//
+//  dataFromClarifai.h
+//  Kaufen 2.0
+//
+//  Created by Deetpanshu Malik on 2/11/17.
+//  Copyright © 2017 Boilermake4_Deeptanshu_Jake_Mason_Ryuji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dataFromClarifai : NSObject
+
+@end
