ca799500978c0b8e6800e87624fd99e653f5d1d7
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-02-11 19:12:24 -0500

    Initial Commit

---
+++ Kaufen 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kaufen 2.0
+//
+//  Created by Deetpanshu Malik on 2/11/17.
+//  Copyright © 2017 Boilermake4_Deeptanshu_Jake_Mason_Ryuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
