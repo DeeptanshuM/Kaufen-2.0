5028fda220ce9271a8b450bca1d3e3d927e2b39e
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-02-12 20:42:05 -0500

    done with UI design for bare bones receipts view controller

---
+++ Kaufen 2.0/getDataFromReceipt.swift
@@ -0,0 +1,36 @@
+//
+//  getDataFromReceipt.swift
+//  Kaufen 2.0
+//
+//  Created by Deetpanshu Malik on 2/12/17.
+//  Copyright © 2017 Boilermake4_Deeptanshu_Jake_Mason_Ryuji. All rights reserved.
+//
+
+import UIKit
+import Clarifai
+
+class getDataFromReceipt: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
